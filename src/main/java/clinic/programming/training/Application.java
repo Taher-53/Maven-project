317499f0db994a23ea1a12a8905c7d367b807961
Taher-53 <vasanwalataher52@gmail.com>
2022-01-20 13:41:59 +0530

    Use strings

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -2,7 +2,13 @@ package clinic.programming.training;
 
 import java.util.Arraylist;
 import java.util.List;
+import org.apache.commons.lang3.StringUtils;
 public class Application {
+
+    public int countWords(String words) {
+        String[] separateWords = StringUtils.split(words, ' ');
+        return (separateWords == null) ? 0 : separateWords.length;
+    }
 
     public void greet(){
         List<String> greetings = new ArrayList<>();
@@ -20,5 +26,7 @@ public class Application {
     	System.out.println ("Starting Application");
 	    Application app = new Application();
         app.greet();
+        int count = app.countWords("I have four words");
+        System.out.println("Word Count: " + count);
     }
 }
